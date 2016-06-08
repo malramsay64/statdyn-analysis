c0ea03dc3f4971edb3d252f23182516fd0eaba7a
malramsay64 <malramsay64@gmail.com>
2016-06-08 18:09:53 +1000

    Initialisatin script for interactive python
    Set up script that sets up and equilibrates the system ready for an
    interactive python session. This makes it much easer to test ideas and
    query the results of the simulations before deiciding what to do next.

---
+++ Trimer-setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+#
+# Malcolm Ramsay 2016-03-09
+#
+
+# Hoomd helper functions
+
+import os.path
+import numpy as np
+from hoomd_script import *
+
+context.initialize()
+
+if init.is_initialized():
+    init.reset()
+
+system = init.read_xml(filename="Trimer-13.50-5.00.xml")
+update.enforce2d()
+
+potentials = pair.lj(r_cut=2.5)
+potentials.pair_coeff.set('1','1', epsilon=1, sigma=2)
+potentials.pair_coeff.set('2','2', epsilon=1, sigma=0.637556*2)
+potentials.pair_coeff.set('1','2', epsilon=1, sigma=1.637556)
+
+
+thermo = analyze.log(filename=None, quantities=['temperature', 'pressure'], period=1000)
+gall = group.all()
+
+
+integrate.mode_standard(dt=0.001)
+npt = integrate.npt_rigid(group=gall, T=2.5, tau=5, P=13.5/2, tauP=5)
+run(10000)
+npt.set_params(tau=1, tauP=1)
+run(10000)
+integrate.mode_standard(dt=0.005)
+
+
+def get_stats(steps=1000, points=200):
+    press = []
+    temp = []
+    for i in range(points):
+        run(steps)
+        temp.append(thermo.query('temperature'))
+        press.append(thermo.query('pressure'))
+    print("Temp -- Mean: {mean} Stddev: {stdev}".format(mean=np.mean(temp), stdev=np.std(temp)))
+    print("Press -- Mean: {mean} Stddev: {stdev}".format(mean=np.mean(press), stdev=np.std(press)))
+    return (np.mean(temp), np.std(temp), np.mean(press), np.std(press))
+
+snapshot = ''
+def run_t_p(temp, press, steps=500000):
+    npt.set_params(T=temp, P=press)
+    run(steps)
+    stats = get_stats()
+    print(stats)
+    global snapshot
+    snapshot = system.take_snapshot(all=True)
+    return stats
+
+
+vals = [(5.0,13.5),
+        (4.0, 10.0),
+        (3.0, 8.5),
+        (4.0, 6.75),
+        (3.0, 6.75),
+        (2.5, 6.75),
+        (2.0, 6.75),
+        (1.8, 6.75),
+        (2.5,5.0),
+        (2.0,4.0),
+        (1.5,3.0),
+        ]
