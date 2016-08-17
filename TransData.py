e5e4d7a17cb70a97f2d8a74e88abcc3597a87577
malramsay64 <malramsay64@gmail.com>
2016-08-17 18:52:09 +1000

    Documenting the TransData module

--- TransData.py
+++ TransData.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-""" A set of classes used for computing the dynamic properties of a Hoomd MD
-simulation"""
+""" A set of classes to store the translational and rotational data from a
+hoomd simulation
+"""
 
 from __future__ import print_function
 import sys
@@ -22,7 +23,7 @@ class TransData(object):
         self.timesteps = 0
 
     def from_trans_array(self, translations, timesteps):
-        """ Initialise the values of the TransData object from a list
+        """ Initialise the values of the TransData object from an array
 
         Args:
             translations (:numpy:`array`): Array containing the precomputed
@@ -37,22 +38,57 @@ class TransData(object):
         self.timesteps = timesteps
 
     def from_json(self, string):
-        """Initialise from JSON string"""
+        """Initialise from JSON string
+
+        This is a string encoded in JSON containing at least a list of the
+        translational motion of each molecule and the timestep. The following
+        is a minimal example ::
+            >>> string = "{'timesteps':1, 'translations':[0.1,0.1,0,0.2,0.2]}"
+            >>> TransData().from_json(string)
+
+        Todo:
+            Deal with error conditions, when the data is incomplete/incorrect
+
+        Args:
+            string (str): String in the JSON format containg the data to be
+                imported. The json requires both a `translations` field and
+                a `timesteps` field.
+        """
         dct = json.loads(string)
         self.trans = np.array(dct["translations"])
         self.timesteps = dct["timesteps"]
 
     def to_json(self, outfile=''):
-        """Convert representation to JSON for writing to a file
+        """Convert representation to JSON string for writing to a file
+
+        This converts the data to a string which can be easily stored
+        in a file for later input and computation. The JSON file structure
+        was chosen due to simplicity in both reading and writing as well
+        as the descriptive nature.
+
+        Note:
+            While each string is the the JSON format the entire file is
+            not in the JSON format. This is due to the expected large size
+            of the files, making it not feasible to store the entire file in
+            memory at the same time. Since it is simple enough to deal with
+            a single frame at a time, each frame will be on a separate line
+            which is in the JSON format and the file can be dealt with line
+            by line.
+
+        Args:
+            outfile (str): Filename to direct data to
         """
         if outfile:
             output = sys.stdout
         else:
-            output = open(outfile, 'w')
-        json.dump({"__TransRotData__":True,
-                   "timesteps":self.timesteps,
-                   "translations":np.ndarray.tolist(self.trans)
-                  }, output, separators=(',', ':'))
+            output = open(outfile, 'a')
+        l_trans = np.array(self.trans)
+        output.write(json.dumps({"__TransRotData__":True,
+                                 "timesteps":self.timesteps,
+                                 "translations":l_trans,
+                                }, output, separators=(',', ':')))
+        output.write('\n')
+        output.close()
 
 
 
@@ -70,14 +106,18 @@ class TransRotData(TransData):
         self.rot = np.array([])
 
     def from_arrays(self, trans, rot, timesteps):
-        """Initialise TransRotData from precomputed arrays
+        """Initialise TransRotData from precomputed :class`numpy.array`
 
         Both the translational and rotational arrays have to have the same
         ordering of molecules i.e. the data in `trans[i]` corresponds to the
         same molecue as `rot[i]`
 
+        Todo:
+            Deal with error condiditions where there are not the same
+            number of molecules etc.
+
         Args:
-            trans (array): Array of all translations
+            trans (nuarray): Array of all translations
             rot (array): Array of all rotations
             timesteps (int): Number of timesteps between initial and final
                 configurations.
@@ -96,14 +136,47 @@ class TransRotData(TransData):
         self.timesteps = timesteps
 
     def from_json(self, string):
-        """Initialise from JSON string"""
+        """Initialise from JSON string
+
+        This is a string encoded in JSON containing at least a list of the
+        translational motion of each molecule, the rotational motion of each
+        molecule and the timestep. The following is a minimal example ::
+            >>> string = "{'timesteps':1, 'translations':[0.1,0.1,0,0.2,0.2],
+            'rotations':[0.1,0.1,0,0.2,0.2]}"
+            >>> TransRotData().from_json(string)
+
+        Todo:
+            Deal with error conditions, when the data is incomplete/incorrect
+
+        Args:
+            string (str): String in the JSON format containg the data to be
+                imported. The json requires a `translations` field, a
+                `rotations` field and a `timesteps` field.
+        """
         dct = json.loads(string)
         self.trans = np.array(dct["translations"])
         self.rot = np.array(dct["rotations"])
         self.timesteps = dct["timesteps"]
 
     def to_json(self, outfile=''):
-        """Convert representation to JSON for writing to a file
+        """Convert representation to JSON string for writing to a file
+
+        This converts the data to a string which can be easily stored
+        in a file for later input and computation. The JSON file structure
+        was chosen due to simplicity in both reading and writing as well
+        as the descriptive nature.
+
+        Note:
+            While each string is the the JSON format the entire file is
+            not in the JSON format. This is due to the expected large size
+            of the files, making it not feasible to store the entire file in
+            memory at the same time. Since it is simple enough to deal with
+            a single frame at a time, each frame will be on a separate line
+            which is in the JSON format and the file can be dealt with line
+            by line.
+
+        Args:
+            outfile (str): Filename to direct data to
         """
         if outfile == '':
             output = sys.stdout
