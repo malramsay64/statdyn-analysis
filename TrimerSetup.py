6fb07bc91a59ae44a17d1fb7c020a9a9f748d045
malramsay64 <malramsay64@gmail.com>
2016-08-31 18:50:11 +1000

    Ensuring TrimerSetup compatible with default install
    Making the TrimerSetup.py file a template enabling others to build off
    for their simulations. This means it generates its own initial
    configuration and rigid bodies.

--- TrimerSetup.py
+++ TrimerSetup.py
@@ -11,98 +11,82 @@ import math
 import numpy as np
 import hoomd
 from hoomd import md
-from TimeDep import TimeDep2dRigid
+from hoomd import deprecated
 
+# Let hoomd search for GPUs to use
 hoomd.context.initialize()
 
-input_file = 'Trimer-13.50-5.00.gsd'
-system = hoomd.init.read_gsd(filename=input_file, time_step=0)
+# Create a 2D square lattice of 50x50 with particles of type A on the lattice
+# points
+system = hoomd.init.create_lattice(unitcell=hoomd.lattice.sq(a=4), n=[50, 50])
 md.update.enforce2d()
+
 # Set moments of inertia for every central particle
 for particle in system.particles:
     if particle.type == 'A':
-        particle.moment_inertia = (1.65, 10, 10)
+        particle.moment_inertia = (0, 0, 1.65)
 
+# Let the system know there is anther type of particle
+system.particles.types.add('B')
 
+# Set interaction potentials for all interaction types
 potentials = md.pair.lj(r_cut=2.5, nlist=md.nlist.cell())
 potentials.pair_coeff.set('A', 'A', epsilon=1, sigma=2)
 potentials.pair_coeff.set('B', 'B', epsilon=1, sigma=0.637556*2)
 potentials.pair_coeff.set('A', 'B', epsilon=1, sigma=1.637556)
 
+# Define rigid bodies
 rigid = md.constrain.rigid()
+# Each rigid body centered on the particles of type A
+# has two particles of type B at the given relative coordinates
 rigid.set_param('A', positions=[(math.sin(math.pi/3),
                                  math.cos(math.pi/3), 0),
                                 (-math.sin(math.pi/3),
                                  math.cos(math.pi/3), 0)],
                 types=['B', 'B']
                )
-rigid.create_bodies(create=False)
+# Create the extra particles
+rigid.create_bodies(create=True)
+# A group only containing the center particles of the rigid bodies
 center = hoomd.group.rigid_center()
 
+# Output thermodynamic data
+thermo = hoomd.analyze.log(
+    filename="out.dat",
+    quantities=[
+        'temperature',
+        'pressure',
+        'volume',
+        'translational_kinetic_energy',
+        'rotational_kinetic_energy',
+        'rotational_ndof',
+        'translational_ndof',
+        'N'
+    ],
+    period=1000
+)
 
-thermo = hoomd.analyze.log(filename=None,\
-                     quantities=['temperature', 'pressure'], \
-                     period=1000 \
-                    )
-
+# Set integration parameters
 md.integrate.mode_standard(dt=0.001)
-npt = md.integrate.npt(group=center, \
-                          kT=2.5, \
-                          tau=5, \
-                          P=13.5/2, \
-                          tauP=5\
-                         )
-hoomd.run(100)
-init = TimeDep2dRigid(system)
-npt.set_params(tau=1, tauP=1)
+npt = md.integrate.npt(
+    group=center,
+    kT=2.0,
+    tau=5,
+    P=13.5,
+    tauP=5
+)
+
 hoomd.run(10000)
+
+# Increase step size and decrease Nose-Hoover imaginary mass
 md.integrate.mode_standard(dt=0.005)
+npt.set_params(tau=1, tauP=1)
 
-
-def get_stats(steps=1000, points=200):
-    """ Calculate statistics for the pressure and temperature
-    of the simulation and compare them to the expected values.
-    param: steps The number of steps between points, defults
-    to 1000
-    param: point The number of points to collect for statistics,
-    defaults to 200
-    """
-    press = []
-    temp = []
-    for i in range(points):
-        hoomd.run(steps)
-        temp.append(thermo.query('temperature'))
-        press.append(thermo.query('pressure'))
-    print("Temp -- Mean: {mean} Stddev: {stdev}"\
-            .format(mean=np.mean(temp), stdev=np.std(temp)))
-    print("Press -- Mean: {mean} Stddev: {stdev}"\
-            .format(mean=np.mean(press), stdev=np.std(press)))
-    return (np.mean(temp), \
-            np.std(temp),  \
-            np.mean(press),\
-            np.std(press)  \
-           )
-
-snapshot = ''
-def run_t_p(temp, press, steps=500000):
-    npt.set_params(kT=temp, P=press)
-    hoomd.run(steps)
-    stats = get_stats()
-    print(stats)
-    global snapshot
-    snapshot = system.take_snapshot(all=True)
-    return stats
-
-
-vals = [(5.0, 13.5),
-        (4.0, 10.0),
-        (3.0, 8.5),
-        (4.0, 6.75),
-        (3.0, 6.75),
-        (2.5, 6.75),
-        (2.0, 6.75),
-        (1.8, 6.75),
-        (2.5, 5.0),
-        (2.0, 4.0),
-        (1.5, 3.0),
-       ]
+# Monitor simulation
+xml = deprecated.dump.xml(filename="out.xml", group=hoomd.group.all(), all=True)
+xml.write("out.xml")
+hoomd.analyze.imd(
+    port = 4321,
+    period = 200,
+)
+hoomd.run(100000)
