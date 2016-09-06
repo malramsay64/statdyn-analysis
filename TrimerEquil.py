41119439fc31aa78d5195f7bddf5a2b8aa222739
malramsay64 <malramsay64@gmail.com>
2016-09-06 14:15:27 +1000

    Increasing steps in equilibration

--- TrimerEquil.py
+++ TrimerEquil.py
@@ -14,20 +14,20 @@ import equil
 PRESS = 13.5
 # Holds tuples of the temperature and number of steps
 # to iterate through
-STEPS = 1000000
+STEPS = 10000000
 TEMPERATURES = [
     (5.00, 1*STEPS),
     (4.00, 1*STEPS),
     (3.50, 1*STEPS),
     (3.00, 1*STEPS),
     (2.50, 2*STEPS),
-    (2.00, 2*STEPS),
-    (1.80, 4*STEPS),
-    (1.60, 4*STEPS),
-    (1.50, 8*STEPS),
-    (1.40, 8*STEPS),
-    (1.35, 16*STEPS),
-    (1.30, 16*STEPS),
+    (2.00, 4*STEPS),
+    (1.80, 8*STEPS),
+    (1.60, 8*STEPS),
+    (1.50, 16*STEPS),
+    (1.40, 32*STEPS),
+    (1.35, 64*STEPS),
+    (1.30, 128*STEPS),
 ]
 
 if __name__ == "__main__":
