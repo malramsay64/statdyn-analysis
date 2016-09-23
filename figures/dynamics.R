bfdef46f26362c359f21d840457bc958fbcd456b
malramsay64 <malramsay64@gmail.com>
2016-09-23 19:28:02 +1000

    Modifying figures for presenting
    I have increased the text size making them easier to see on a screen,
    also dealing with the theme of the figures making it clearer.

--- figures/dynamics.R
+++ figures/dynamics.R
@@ -42,18 +42,22 @@ collated_av <- aggregate(collated[,!(names(collated) %in% c("temp", "time"))],
 collated_trans <- aggregate(collated_trans[,!(names(collated_trans) %in% c("temp", "time"))],
                          list(temp=collated_trans$temp, time=collated_trans$time), mean)
 
+collated_av <- subset(collated_av, collated_av$temp >= 1.40)
+
 p <- ggplot(collated_av, aes(x=time*timestep, colour=temp)) + scale_x_log10() + labs(x="Time")
+p <- p + theme_bw(base_size=19) + scale_color_discrete("Temperature")
 
 msd <- p + geom_path(aes(y=msd))
 msd <- msd + scale_y_log10() + labs(y=expression(symbol("\341")* Delta*r^2* symbol("\361")))
 
-alpha <- p + geom_path(aes(y=alpha)) + labs(y=expression(frac(symbol("\341") *Delta *r^4 *symbol("\361"), 2*symbol("\341")* Delta *r^2 *symbol("\361")^2) -1))
+alpha <- p + geom_path(aes(y=alpha)) + labs(y=expression(alpha))
+#+ labs(y=expression(frac(symbol("\341") *Delta *r^4 *symbol("\361"), 2*symbol("\341")* Delta *r^2 *symbol("\361")^2) -1))
 
 decoupling <- p + geom_path(aes(y=decoupling))
 
 g1 <- expression(frac(symbol("\341") *Delta *r *Delta*theta* symbol("\361") - symbol("\341")* Delta*r*symbol("\361")* symbol("\341")* Delta*theta*symbol("\361"), symbol("\341")* Delta * r *symbol("\361")* symbol("\341") *Delta * theta *symbol("\361")))
 g2 <- expression(frac(symbol("\341") *(Delta *r *Delta*theta)^2* symbol("\361") - symbol("\341")* Delta*r^2*symbol("\361")* symbol("\341")* Delta*theta^2*symbol("\361"), symbol("\341")* Delta * r^2 *symbol("\361")* symbol("\341") *Delta * theta^2* symbol("\361")))
-gamma1 <- p + geom_path(aes(y=gamma1)) + labs(y=g1)
+gamma1 <- p + geom_path(aes(y=gamma1)) + labs(y=expression(gamma[1]))
 
 gamma2 <- p + geom_path(aes(y=gamma2)) + labs(y=g2)
 
@@ -120,10 +124,12 @@ struct_rplot <- struct_rplot + labs(x="1/T", y="Structural Relaxation Time")
 
 max_g1 <- list()
 max_g2 <- list()
+max_alpha <- list()
 for (t in unique(collated_av$temp)) {
     s <- subset(collated_av, collated_av$temp == t)
     max_g1[t] <- max(s$gamma1)
     max_g2[t] <- max(s$gamma2)
+    max_alpha[t] <- max(s$alpha)
 }
 m_gamma_df <- data.frame(temp=as.numeric(names(max_g1)), max_g1=as.numeric(max_g1), max_g2=as.numeric(max_g2))
 m_g <- ggplot(m_gamma_df, aes(x=1/temp)) + labs(x="1/T")
@@ -132,7 +138,7 @@ mg1 <- expression(max(frac(symbol("\341") *Delta *r *Delta*theta* symbol("\361")
 mg2 <- expression(max(frac(symbol("\341") *(Delta *r *Delta*theta)^2* symbol("\361") - symbol("\341")* Delta*r^2*symbol("\361")* symbol("\341")* Delta*theta^2*symbol("\361"), symbol("\341")* Delta * r^2 *symbol("\361")* symbol("\341") *Delta * theta^2* symbol("\361"))))
 m_g1 <- m_g + geom_line(aes(y=max_g1)) + geom_point(aes(y=max_g1)) + labs(y=mg1)
 m_g2 <- m_g + geom_line(aes(y=max_g2)) + geom_point(aes(y=max_g2)) + labs(y=mg2)
-
+m_alpha <- m_g + geom_line(aes(y=max_alpha)) + geom_point(aes(y=max_alpha)) + labs(y=expression(max(alpha))
 
 D = list()
 for (t in unique(collated_av$temp)) {
@@ -140,7 +146,7 @@ for (t in unique(collated_av$temp)) {
     D[t] = s$time[[Position(function(i) i < 1/exp(1),s$rot2)]]
 }
 
-pdf("dynamics.pdf", width=8, height=6)
+pdf("dynamics.pdf", width=10, height=6)
 print(msd)
 print(alpha)
 print(decoupling)
@@ -158,4 +164,13 @@ print(rot2_rplot)
 print(struct_rplot)
 print(m_g1)
 print(m_g2)
+print(m_alpha)
+dev.off()
+
+pdf("gamma1.pdf", width=10, height=6)
+print(gamma1)
+dev.off()
+
+pdf("alpha.pdf", width=10,height=6)
+print(alpha)
 dev.off()
