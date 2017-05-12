52d1ca64c4f5fe6627fa2ab53a952ab15d79d3b6
Malcolm Ramsay <malramsay64@gmail.com>
2017-05-12 17:51:29 +1000

    Optimise generate_steps function
    Both optimising the generation of steps by using a simple python
    generator. I have also debugged the generator so that it no longer has
    off by one errors.
    
    Along with the optimisations of the code I have also used the numba it
    which shows small optimisations to the speed in tests generating a list
    from the generator. I don't actually know if this will have real work
    implications, but it is a simple change.

--- StepSize.py
+++ StepSize.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python
 """ A series of classes that specify various step size functions"""
 
-import math
+from numba import jit
 
+
+@jit
 def generate_steps(total_steps, num_linear=99, start=0):
-    """Generate a sequence of steps"""
-    curr_step = start
-    step_size = 1
-    lin_steps = 0
-    while curr_step < total_steps:
-        while lin_steps < num_linear:
-            curr_step += step_size
-            lin_steps += 1
-            if curr_step < total_steps:
-                yield curr_step
-            else:
-                yield total_steps
-        step_size *= 10
-
-
-class PowerSteps(object):
     """Generate a sequence of steps with a power law
 
     A sequence of steps which consisting of points spaced by a
@@ -35,37 +21,19 @@ class PowerSteps(object):
 
     Example:
 
-        >>> p = PowerSteps(9,1)
-        >>> [ p.next() for i in range(19) ]
+        >>> [s for s in generate_steps(100, 9)]
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
     """
-    def __init__(self, num_linear=9, pow_jump=1, start=0):
-        self.num_linear = num_linear
-        self.pow_jump = pow_jump
-        self.power = 0
-        self.curr_step = 0
-        self.delta_step = self.get_delta_step()
-        self.start = start
-
-    def next(self):
-        """ Calculate the next value in the series"""
-        self.curr_step = int(self.curr_step + self.delta_step)
-        if self.curr_step >= (self.num_linear+1)*math.pow(10, self.power):
-            self.power += 1
-            self.curr_step = (self.num_linear+1)*int(math.pow(10, self.power-1))
-            self.delta_step = self.get_delta_step()
-        return self.curr_step + self.start
-
-    def __next__(self):
-        return self.next()
-
-    def get_delta_step(self):
-        """ Calculate the step size"""
-        delta_s = (math.pow(10, self.power))
-        if delta_s < 1:
-            delta_s = 1
-        return delta_s
-
-    def __lt__(self, other):
-        return self.get_delta_step() < other.get_delta_step()
+    curr_step = start
+    step_size = 1
+    lin_steps = 0
+    curr_step += step_size
+    while curr_step < total_steps:
+        if lin_steps == num_linear:
+            step_size *= 10
+            lin_steps = 0
+        yield curr_step
+        lin_steps += 1
+        curr_step += step_size
+    yield total_steps
