a0e9fb77dea0208b659a3a4871af2bd697a8b707
Malcolm Ramsay <malramsay64@gmail.com>
2017-05-18 12:52:51 +1000

    Put the execution of the simulation in a function
    Moving the execution of the simulation run into a function which makes
    it much simpler to call many times with new starting configurations.

--- basic.py
+++ basic.py
@@ -8,48 +8,33 @@ import numpy as np
 import pandas
 import TimeDep
 from hoomd import md
+import gsd.hoomd
 from StepSize import generate_steps
 
 
-def randomise(snapshot):
-    """Randomise the momentum of the snapshot
-
-    The randomisation is performed by shuffing the rotational and translational
-    momenta independantly of each other. Where there are rigid bodies in the
-    snapshot only the momenta of the centers are shuffled.
-
-    Args:
-        shapshot (class:`hoomd.data.snapshot`): Snapshot of which to shuffle
-            momenta
-
-    Returns:
-        class:`hoomd.data.snapshot`: A new snapshot with randomised momenta
-    """
-    temp = hoomd.context.initialize('--notice-level=0')
-    with temp:
-        system = hoomd.init.read_snapshot(snapshot)
-        mysnap = system.take_snapshot(all=True)
-    nbodies = mysnap.particles.body.max() + 1
-    np.random.shuffle(mysnap.particles.velocity[:nbodies])
-    np.random.shuffle(mysnap.particles.angmom[:nbodies])
-    return mysnap
-
-
-def initialise(snapshot):
+def run_npt(snapshot, temp, steps, **kwargs):
     """Initialise a hoomd simulation"""
-    context = hoomd.context.initialize('--notice-level=0')
-    with context:
+    with hoomd.context.initialize(kwargs.get('init_args', '')):
         system = hoomd.init.read_snapshot(snapshot)
         md.update.enforce2d()
-        mol = molecule.Trimer()
+        mol = kwargs.get('mol', molecule.Trimer())
         mol.initialize(create=False)
-        md.integrate.mode_standard(dt=0.005)
-        md.integrate.npt(group=hoomd.group.rigid_center(),
-                         kT=1.30, tau=1, P=13.50, tauP=1)
-    return context, system
+        md.integrate.mode_standard(kwargs.get('dt', 0.005))
+        md.integrate.npt(
+            group=hoomd.group.rigid_center(),
+            kT=temp,
+            tau=kwargs.get('tau', 1.),
+            P=kwargs.get('press', 13.5),
+            tauP=kwargs.get('tauP', 1.)
+        )
+        dynamics = TimeDep.TimeDep2dRigid(snapshot, 0)
+        for curr_step in generate_steps(steps):
+            hoomd.run_upto(curr_step)
+            dynamics.append(system.take_snapshot(all=True), curr_step)
+        return dynamics.get_all_data()
 
 
-def read_snapshot(fname):
+def read_snapshot(fname, rand=False):
     """Read a hoomd snapshot from a hoomd gsd file
 
     Args:
@@ -58,35 +43,23 @@ def read_snapshot(fname):
     Returns:
         class:`hoomd.data.Snapshot`: Hoomd snapshot
     """
-    with hoomd.context.initialize('--notice-level=0'):
-        system = hoomd.init.read_gsd(fname)
-        return system.take_snapshot(all=True)
+    with gsd.hoomd.open(fname) as trj:
+        snapshot = trj.read_frame(0)
+        if rand:
+            snapshot.particles.angmom
+            nbodies = snapshot.particles.body.max() + 1
+            np.random.shuffle(snapshot.particles.velocity[:nbodies])
+            np.random.shuffle(snapshot.particles.angmom[:nbodies])
+        return snapshot
 
 
-def main(init_file, steps):
+def main(init_file, temp, steps):
     """Main function to run stuff"""
-    snap1 = read_snapshot(init_file)
-    snap2 = randomise(snap1)
-    context1, sys1 = initialise(snap1)
-    context2, sys2 = initialise(snap2)
-    with context1:
-        dynamics1 = TimeDep.TimeDep2dRigid(snap1, 0)
-        for curr_step in generate_steps(steps):
-            hoomd.run_upto(curr_step)
-            dynamics1.append(sys1.take_snapshot(all=True), curr_step)
-        fin1 = sys1.take_snapshot(all=True)
-    with context2:
-        dynamics2 = TimeDep.TimeDep2dRigid(snap2, 0)
-        for curr_step in generate_steps(steps):
-            hoomd.run_upto(curr_step)
-            dynamics2.append(sys2.take_snapshot(all=True), curr_step)
-        fin2 = sys2.take_snapshot(all=True)
+    dynamics1 = run_npt(read_snapshot(init_file, rand=True), temp, steps)
+    dynamics2 = run_npt(read_snapshot(init_file, rand=True), temp, steps)
     with pandas.HDFStore(os.path.splitext(init_file)[0]+'.hdf5') as store:
         store['dyn1'] = dynamics1.get_all_data()
         store['dyn2'] = dynamics2.get_all_data()
-    return (snap1, fin1, dynamics1.get_all_data(),
-            snap2, fin2, dynamics2.get_all_data())
-
 
 if __name__ == '__main__':
     main("./Trimer-13.50-1.30.gsd", 1000)
