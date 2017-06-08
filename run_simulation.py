eb532bc6f766e5f99e730d4b8025828a34df209d
Malcolm Ramsay <malramsay64@gmail.com>
2017-06-08 20:59:02 +1000

    Add many new cmdline args to run_simulation
    There is a lot of functionality I hadn't unlocked through the command
    line tool, which is now available. This includes running crystal
    simulations, and being able to toggle the output of thermodynamic data

--- run_simulation.py
+++ run_simulation.py
@@ -17,6 +17,10 @@ import pandas
 from statdyn import initialise, Simulation, crystals
 
 
+crystal_funcs={
+    'p2': crystals.p2,
+}
+
 def get_temp(fname):
     from os.path import split, splitext
     fname = split(fname)[1]
@@ -34,21 +38,24 @@ def crystalline():
     Simulation.run_npt(snapshot, 0.1, 1)
 
 
-def main(steps, temp, directory, output, thermo=True):
-    if glob.glob(directory+'/'+initialise.get_fname(temp)):
+def main(steps, temp, directory, output, thermo=True, init_crys=None, lattice_lengths=(30, 40)):
+    if init_crys:
+        crys = crystal_funcs.get(init_crys)
+        snapshot = initialise.init_from_crystal(crys(), cell_dimensions=lattice_lengths).take_snapshot()
+    elif glob.glob(directory+'/'+initialise.get_fname(temp)):
         snapshot = initialise.init_from_file(
             directory+'/'+initialise.get_fname(temp)).take_snapshot()
     else:
         snapshot = initialise.init_from_file(directory+'/'+initialise.get_fname(
             get_closest(temp, directory))).take_snapshot()
-    data = Simulation.run_multiple_concurrent(
+    data = Simulation.run_npt(
         snapshot,
         temp,
         steps,
         thermo=thermo
     )
     os.makedirs(output, exist_ok=True)
-    with pandas.HDFStore(output+'/'+get_fname(temp, 'hdf5')) as dst:
+    with pandas.HDFStore(output+'/'+initialise.get_fname(temp, 'hdf5')) as dst:
         dst['dynamics'] = data
 
 
@@ -57,6 +64,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '-s',
         '--steps',
+        required=True,
         type=int,
         help='The number of steps to run the simulation for'
     )
@@ -64,25 +72,47 @@ if __name__ == "__main__":
         '-t',
         '--temp',
         type=float,
+        required=True,
         help='The temperature of the simulation'
     )
     parser.add_argument(
         '-d',
         '--dir',
         type=str,
+        default='.',
         help='The directory in which to find the gsd configuration files'
     )
     parser.add_argument(
         '-o',
         '--output',
         type=str,
+        default='.',
         help='The directory to store the log files and data files'
     )
     parser.add_argument(
         '--no-thermo',
+        dest='thermo',
         action='store_false',
         help='Do not output thermodynamic data to a file'
     )
+    parser.add_argument(
+        '--thermo',
+        dest='thermo',
+        action='store_true',
+        help='Output thermodynamics data to a file (defualt)'
+    )
+    parser.add_argument(
+        '--init-crys',
+        default=None,
+        help='Initialise the simulation from a crystal'
+    )
+    parser.add_argument(
+        '--lattice-lengths',
+        nargs=2,
+        default=(30, 40),
+        help='Number of repetitions in the a and b lattice vectors'
+    )
+    parser.set_defaults(thermo=True)
     args = parser.parse_args()
-    main(args.steps, args.temp, args.dir, args.output)
+    main(args.steps, args.temp, args.dir, args.output, args.thermo, args.init_crys, args.lattice_lengths)
 
