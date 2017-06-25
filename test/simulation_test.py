c190bebd26ae99b6a9a3b19f27e3246ced561881
Malcolm Ramsay <malramsay64@gmail.com>
2017-06-25 16:13:39 +1000

    Test files in correct location
    I have added a test case to simulation_test that ensures that neither
    files are created in the current directory and that the appropriate
    files are created in the output directory.

--- test/simulation_test.py
+++ test/simulation_test.py
@@ -8,16 +8,22 @@
 
 """Test the simulation module."""
 
+import os
+from pathlib import Path
+
 import pytest
 from statdyn import Simulation, crystals, initialise
 
 from .crystal_test import CELL_DIMS
 
+OUTDIR = Path('test/tmp')
+OUTDIR.mkdir(exist_ok=True)
+
 
 def test_run_npt():
     """Test an npt run."""
     snapshot = initialise.init_from_none().take_snapshot()
-    Simulation.run_npt(snapshot, 3.00, 100, dyn_many=False)
+    Simulation.run_npt(snapshot, 3.00, 100, dyn_many=False, output=OUTDIR)
     assert True
 
 
@@ -26,7 +32,7 @@ def test_run_multiple_concurrent(dyn_many):
     """Test running multiple concurrent."""
     snapshot = initialise.init_from_file(
         'test/data/Trimer-13.50-3.00.gsd').take_snapshot()
-    Simulation.run_npt(snapshot, 3.00, 100, dyn_many=dyn_many)
+    Simulation.run_npt(snapshot, 3.00, 100, dyn_many=dyn_many, output=OUTDIR)
     assert True
 
 
@@ -36,8 +42,11 @@ def test_thermo():
     There are many thermodynamic values set in the function and ensuring that
     they can all be initialised is crucial to a successful simulation.
     """
+    output = Path('test/tmp')
+    output.mkdir(exist_ok=True)
     snapshot = initialise.init_from_none().take_snapshot()
-    Simulation.run_npt(snapshot, 3.00, 100, thermo=True, thermo_period=1)
+    Simulation.run_npt(snapshot, 3.00, 100, thermo=True, thermo_period=1,
+                       output=OUTDIR)
     assert True
 
 
@@ -48,8 +57,25 @@ def test_orthorhombic_sims(cell_dimensions):
     This also ensures there is no unusual things going on with the calculation
     of the orthorhombic unit cell.
     """
+    output = Path('test/tmp')
+    output.mkdir(exist_ok=True)
     snap = initialise.init_from_crystal(crystals.TrimerP2(),
-                                        cell_dimensions=cell_dimensions
+                                        cell_dimensions=cell_dimensions,
+                                        output=OUTDIR,
                                         ).take_snapshot()
     Simulation.run_npt(snap, 0.1, 10)
     assert True
+
+
+def test_file_placement():
+    """Ensure files are located in the correct directory when created."""
+    outdir = Path('test/output')
+    outdir.mkdir(exist_ok=True)
+    current = list(Path('.').glob('*'))
+    _ = [os.remove(i) for i in outdir.glob('*')]
+    snapshot = initialise.init_from_none().take_snapshot()
+    Simulation.run_npt(snapshot, 3.00, 10, dyn_many=False, output=outdir)
+    assert current == list(Path('.').glob('*'))
+    assert (outdir / 'Trimer-13.50-3.00.gsd').is_file()
+    assert (outdir / 'dump-13.50-3.00.gsd').is_file()
+    assert (outdir / 'thermo-13.50-3.00.log').is_file()
