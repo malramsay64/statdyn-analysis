6203d33555d73c49b1510ea6609261aca424130f
Malcolm Ramsay <malramsay64@gmail.com>
2017-06-26 15:18:09 +1000

    Add function to compute the Spearman rank correlation

--- statdyn/datagen/collate.py
+++ statdyn/datagen/collate.py
@@ -26,6 +26,27 @@ def transrot(trans: np.array, rot: np.array) -> float:
     return 0
 
 
+def spearman_rank(trans: np.array, rot: np.array) -> float:
+    """Compute the Spearman Rank coefficient for fast molecules.
+
+    This takes the molecules with the fastest 10% of the translations or
+    rotations and uses this subset to compute the Spearman rank coefficient.
+    """
+    t_order = np.argsort(trans)
+    r_order = np.argsort(rot)
+    num_elements = int(t_order.shape[0] / 10)
+    argmotion = np.union1d(t_order[:num_elements], r_order[:num_elements])
+    t_order_small = t_order[np.in1d(t_order, argmotion, assume_unique=True)]
+    r_order_small = r_order[np.in1d(r_order, argmotion, assume_unique=True)]
+    num_elements = len(t_order_small)
+    diff = np.zeros(num_elements)
+    for index, val in enumerate(r_order_small.index.values):
+        diff[index] = np.square(index - np.where(
+            t_order_small.index.values == val)[0][0])
+    return 1 - ((diff.sum() * 6) /
+                (num_elements * (np.square(num_elements) - 1)))
+
+
 def allgroups(trans: pandas.DataFrame,
               rot: pandas.DataFrame=None,
               func: Callable[[np.array, np.array], float]=transrot
