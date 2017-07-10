5714fd6973cc8018bc1a33b9a51e1d033464189e
Malcolm Ramsay <malramsay64@gmail.com>
2017-07-10 19:34:58 +1000

    Ensure build works from clean repository

---
+++ setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Command line tool to run simulations."""
+
+from setuptools import setup
+
+
+setup(
+    name='statdyn',
+    version='0.1',
+    py_modules=['statdyn'],
+    install_requires=[
+        'Click',
+        'numpy',
+        'pandas',
+    ],
+    entry_points="""
+        [console_scripts]
+        sdrun=statdyn.sdrun.main:main
+    """,
+)
