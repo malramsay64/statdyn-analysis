c5619977fe566b61f21a5a9424bf540b12dd90e0
Malcolm Ramsay <malramsay64@gmail.com>
2017-07-12 16:43:07 +1000

    Bugfix - build and install working correctly

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@
 
 """Command line tool to run simulations."""
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='statdyn',
     version='0.1',
-    py_modules=['statdyn'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
     ],
