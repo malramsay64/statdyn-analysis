c6b2c7ccda83a923a0ac0d8733f3332706514e4e
Malcolm Ramsay <malramsay64@gmail.com>
2017-07-14 15:41:02 +1000

    Refactor options into separate file
    There are many different options so to make it clearer I am moving them
    all to a file options.py which is then imported by main.py.

--- statdyn/sdrun/main.py
+++ statdyn/sdrun/main.py
@@ -12,137 +12,14 @@ import logging
 from pathlib import Path
 
 import click
-import hoomd
 
+from . import options
 from .. import crystals
 from ..simulation import initialise, simrun
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logging.basicConfig(level=logging.INFO)
-
-
-def get_temp(fname: Path) -> float:
-    """Convert a filename to a temperature."""
-    return float(fname.stem.split('-')[2])
-
-
-def get_closest(temp: float, directory: Path) -> float:
-    """Find the closet already equilibrated temperature."""
-    files = directory.glob('*.gsd')
-    return temp + min(
-        [get_temp(t) - temp for t in files if get_temp(t) > temp])
-
-
-def crystalline():
-    """Run a crystalline simulation."""
-    snapshot = initialise.init_from_crystal(crystals.TrimerP2).take_snapshot()
-    simrun.run_npt(snapshot, 0.1, 1)
-
-
-def get_initial_snapshot(**kwargs) -> hoomd.data.SnapshotParticleData:
-    """Create the appropriate initial snapshot based on kwargs."""
-    if kwargs.get('init_crys'):
-        crys = CRYSTAL_FUNCS.get(kwargs.get('init_crys'))
-        return initialise.init_from_crystal(
-            crys(),
-            cell_dimensions=kwargs.get('lattice_lengths'),
-            init_args=kwargs.get('init_args'),
-        ).take_snapshot()
-
-    infile = kwargs.get('dir') / initialise.get_fname(kwargs.get('temp'))
-    if infile.is_file():
-        return initialise.init_from_file(
-            infile,
-            init_args=kwargs.get('init_args'),
-        ).take_snapshot()
-    else:
-        return initialise.init_from_file(
-            kwargs.get('dir') / initialise.get_fname(
-                get_closest(kwargs.get('temp'), kwargs.get('dir')))
-        ).take_snapshot()
-
-
-def _mkdir_ifempty(ctx, param, value):
-    if not value or ctx.resilient_parsing:
-        return
-    chkpath = Path(value)
-    logging.debug(f"Directory {value}, {chkpath}")
-    chkpath.mkdir(exist_ok=True)
-    return chkpath
-
-
-def _verbosity(ctx, param, count):
-    if count:
-        logger.info('Setting log level to DEBUG')
-        logging.basicConfig(level=logging.DEBUG)
-
-
-opt_space_group = click.option(
-    '--space-group',
-    default='p2',
-    type=click.Choice(crystals.CRYSTAL_FUNCS.keys())
-)
-
-
-opt_lattice_lengths = click.option(
-    '--lattice-lengths',
-    nargs=2,
-    default=(30, 40),
-    type=(int, int),
-    help='Number of repetitiions in the a and b lattice vectors'
-)
-
-
-opt_configurations = click.option(
-    '-c',
-    '--configurations',
-    default='.',
-    type=click.Path(exists=True),
-    help='location of configurations directory',
-)
-
-
-opt_output = click.option(
-    '-o',
-    '--output',
-    type=click.Path(file_okay=False, writable=True,),
-    callback=_mkdir_ifempty,
-    default='.'
-)
-
-
-opt_verbose = click.option(
-    '-v',
-    '--verbose',
-    count=True,
-    expose_value=False,
-    callback=_verbosity
-)
-
-
-opt_dynamics = click.option(
-    '--dynamics/--no-dynamics',
-    default=True
-)
-
-
-opt_steps = click.option(
-    '-s',
-    '--steps',
-    type=click.IntRange(min=0, max=1e12),
-    required=True,
-    help='Number of steps to run simulation for'
-)
-
-
-opt_temperature = click.option(
-    '-t',
-    '--temperature',
-    type=float,
-    required=True,
-    help='Temperature for simulation'
-)
 
 
 @click.group(name='sdrun')
@@ -165,15 +42,16 @@ def sdrun(ctx):
 
 
 @sdrun.command()
-@opt_space_group
-@opt_lattice_lengths
-@opt_steps
-@opt_temperature
-@opt_dynamics
-@opt_output
-@opt_verbose
+@options.opt_space_group
+@options.opt_lattice_lengths
+@options.opt_steps
+@options.opt_temperature
+@options.opt_dynamics
+@options.opt_output
+@options.opt_verbose
+@options.opt_hoomd_args
 def crystal(space_group, lattice_lengths, steps,
-            temperature, dynamics, output):
+            temperature, dynamics, output, hoomd_args):
     """Run simulations on crystals."""
     snapshot = initialise.init_from_crystal(
         crystals.CRYSTAL_FUNCS.get(space_group)(),
@@ -189,13 +67,14 @@ def crystal(space_group, lattice_lengths, steps,
 
 
 @sdrun.command()
-@opt_steps
-@opt_temperature
-@opt_output
-@opt_configurations
-@opt_verbose
-@opt_dynamics
-def liquid(steps, temperature, output, configurations, dynamics):
+@options.opt_steps
+@options.opt_temperature
+@options.opt_output
+@options.opt_configurations
+@options.opt_verbose
+@options.opt_dynamics
+@options.opt_hoomd_args
+def liquid(steps, temperature, output, configurations, dynamics, hoomd_args):
     """Run simulations on liquid."""
     logger.debug(f'running liquid')
     infile = Path(configurations) / initialise.get_fname(temperature)
@@ -212,4 +91,4 @@ def liquid(steps, temperature, output, configurations, dynamics):
 
 
 if __name__ == "__main__":
-    main()
+    sdrun()
---
+++ statdyn/sdrun/options.py
@@ -0,0 +1,120 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Options for sdrun."""
+
+import logging
+from pathlib import Path
+
+import click
+
+from ..crystals import CRYSTAL_FUNCS
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+
+
+def _mkdir_ifempty(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    chkpath = Path(value)
+    logging.debug(f"Directory {value}, {chkpath}")
+    chkpath.mkdir(exist_ok=True)
+    return chkpath
+
+
+def _verbosity(ctx, param, count):
+    if not count or ctx.resilient_parsing:
+        return
+    logger.info('Setting log level to DEBUG')
+    logging.basicConfig(level=logging.DEBUG)
+
+
+opt_space_group = click.option(
+    '--space-group',
+    default='p2',
+    type=click.Choice(CRYSTAL_FUNCS.keys()),
+    help='Space group of initial crystal.',
+)
+
+
+opt_lattice_lengths = click.option(
+    '--lattice-lengths',
+    nargs=2,
+    default=(30, 40),
+    type=(int, int),
+    help='Number of repetitiions in the a and b lattice vectors',
+)
+
+
+opt_configurations = click.option(
+    '-c',
+    '--configurations',
+    default='.',
+    type=click.Path(exists=True),
+    help='location of configurations directory',
+)
+
+
+opt_output = click.option(
+    '-o',
+    '--output',
+    default='.',
+    type=click.Path(file_okay=False, writable=True,),
+    callback=_mkdir_ifempty,
+    help='Directory to which output files will be written.',
+)
+
+
+opt_verbose = click.option(
+    '-v',
+    '--verbose',
+    count=True,
+    expose_value=False,
+    callback=_verbosity,
+    help='Enable debug logging flags.',
+)
+
+
+opt_dynamics = click.option(
+    '--dynamics/--no-dynamics',
+    default=True,
+    help='''Enable/diable the collection of dynamics quantities.
+
+Enabling the dynamics quantities will collect in addition to the standard dump
+file another trajectory in which the configurations are saved on a logarithmic
+scale.
+'''
+)
+
+
+opt_steps = click.option(
+    '-s',
+    '--steps',
+    type=click.IntRange(min=0, max=1e12),
+    required=True,
+    help='Number of steps to run simulation for.'
+)
+
+
+opt_temperature = click.option(
+    '-t',
+    '--temperature',
+    type=float,
+    required=True,
+    help='Temperature for simulation',
+)
+
+
+opt_hoomd_args = click.option(
+    '--hoomd-args',
+    type=str,
+    default='',
+    help='Arguments to pass to hoomd on context.initialize',
+)
