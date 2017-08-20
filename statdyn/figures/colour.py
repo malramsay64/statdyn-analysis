0daa4a51be1e371eb919a8939c95d8b3d10c5dda
Malcolm Ramsay <malramsay64@gmail.com>
2017-08-20 14:32:44 +1000

    Remove unnecessary code from colour

--- statdyn/figures/colour.py
+++ statdyn/figures/colour.py
@@ -12,24 +12,12 @@ import logging
 
 import numpy as np
 from hsluv import hpluv_to_hex
-from quaternion import as_quat_array, as_rotation_vector
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 HEX_VALUES_DARK = np.array([hpluv_to_hex((value, 85, 65)) for value in range(360)])
 HEX_VALUES_LIGHT = np.array([hpluv_to_hex((value, 85, 85)) for value in range(360)])
-
-
-# def clean_orientation(snapshot):
-    # """Convert an orientation to a sensible format."""
-    # nmol = max(snapshot.particles.body)+1
-    # o_dict = {body: orient for body, orient in zip(
-        # snapshot.particles.body[:nmol],
-        # orientation[:nmol]
-    # )}
-    # orientation = np.array([o_dict[body] for body in snapshot.particles.body])
-    # return orientation
 
 
 def colour_orientation(orientations, light_colours=False):
