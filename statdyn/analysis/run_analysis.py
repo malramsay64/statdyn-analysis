879a9034b134b542d5e5eed69929b68cb6471390
Malcolm Ramsay <malramsay64@gmail.com>
2017-08-21 08:47:45 +1000

    Add error handling for corrupted trajectory frames
    When the analysis came across corrupted frames from a trajectory the
    analysis stopped. Rather than stopping this should just continue past
    the corrupted frames.

--- statdyn/analysis/run_analysis.py
+++ statdyn/analysis/run_analysis.py
@@ -8,12 +8,17 @@
 
 """Run an analysis on a trajectory."""
 
+import logging
+
 import click
 import gsd.hoomd
 import numpy as np
 
 from ..sdrun import options
 from .order import orientational_order
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 @click.command()
@@ -24,7 +29,12 @@ def order(infile, outfile):
     trajectory = gsd.hoomd.open(infile, 'rb')
     with open(outfile, 'w') as dst:
         print('Timestep, OrientOrder', file=dst)
-        for snapshot in trajectory:
+        for index in range(len(trajectory)):
+            try:
+                snapshot = trajectory[index]
+            except RuntimeError:
+                logger.info(f'Frame {index} corrupted, continuing...')
+                continue
             order = orientational_order(snapshot)
             print(f'{snapshot.configuration.step}, {np.sum(order > 0.9) / len(order)}',
                   file=dst)
