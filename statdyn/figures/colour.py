42cb96833b71365745aa2a5a741bfe5eeb506098
Malcolm Ramsay <malramsay64@gmail.com>
2017-08-25 17:15:16 +1000

    Revert deletion of clean_orientation function
    The clean_orientation function is needed for the active configuration
    plot. I am yet to change that to using the plot function.

--- statdyn/figures/colour.py
+++ statdyn/figures/colour.py
@@ -13,11 +13,25 @@ import logging
 import numpy as np
 from hsluv import hpluv_to_hex
 
+from ..analysis.order import get_z_orientation
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 HEX_VALUES_DARK = np.array([hpluv_to_hex((value, 85, 65)) for value in range(360)])
 HEX_VALUES_LIGHT = np.array([hpluv_to_hex((value, 85, 85)) for value in range(360)])
+
+
+def clean_orientation(snapshot):
+    """Convert an orientation to a sensible format."""
+    orientations = get_z_orientation(snapshot.particles.orientation)
+    nmol = max(snapshot.particles.body)+1
+    o_dict = {body: orient for body, orient in zip(
+        snapshot.particles.body[:nmol],
+        orientations
+    )}
+    orientation = np.array([o_dict[body] for body in snapshot.particles.body])
+    return orientation
 
 
 def colour_orientation(orientations, light_colours=False):
