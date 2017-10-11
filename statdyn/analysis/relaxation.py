30a102081da43013de825eb53fd95eeb178066e1
Malcolm Ramsay <malramsay64@gmail.com>
2017-10-11 15:10:25 +1100

    Compute relaxation quantities
    New functions to compute the relaxation of the diffusion constant along
    with exponentially relaxing function like the rotational and structural
    relaxation time. All these functions will return an error estimating the
    precision of the values calculated.
    
    The computation of the relaxation quantities relies heavily on the scipy
    library.

---
+++ statdyn/analysis/relaxation.py
@@ -0,0 +1,128 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""These are a series of summary values of the dynamics quantities.
+
+This provides methods of easily comparing values across variables.
+"""
+
+import logging
+from typing import Tuple
+
+import numpy as np
+from scipy.optimize import curve_fit, newton
+
+logger = logging.getLogger(__name__)
+
+
+def _msd_function(x: np.ndarray, m: float, b: float) -> np.ndarray:
+    return m*x + b
+
+
+def _exponential_decay(x: np.ndarray, a: float, b: float, c: float=0) -> np.ndarray:
+    return a * np.exp(-b * x) + c
+
+
+def _ddx_exponential_decay(x: np.ndarray, a: float, b: float, c: float=0) -> np.ndarray:
+    return -b * a * np.exp(-b * x)
+
+
+def _d2dx2_exponential_decay(x: np.ndarray, a: float, b: float, c: float=0) -> np.ndarray:
+    return b * b * a * np.exp(-b * x)
+
+
+def diffusion_constant(time: np.ndarray,
+                       msd: np.ndarray,
+                       sigma: np.ndarray=None,
+                       ) -> Tuple[float, float]:
+    """Compute the diffusion_constant from the mean squared displacement.
+
+    Args:
+        time (class:`np.ndarray`): The timesteps corresponding to each msd value.
+        msd (class:`np.ndarray`): Values of the mean squared displacement
+
+    Returns:
+        diffusion_constant (float): The diffusion constant
+        error (float): The error in the fit of the diffusion constant
+        (float, float): The diffusion constant
+
+    """
+    linear_region = msd > 2
+    popt, pcov = curve_fit(_msd_function, time[linear_region], msd[linear_region])
+    perr = 2*np.sqrt(np.diag(pcov))
+    return popt[0], perr[0]
+
+
+def simple_exponential_relaxation(time: np.ndarray,
+                                  value: np.ndarray,
+                                  sigma: np.ndarray=None,
+                                  ) -> Tuple[float, float]:
+    """Compute the exponential relaxation of a function by finding the crossover point.
+
+    We are defining exponential relaxation as the time taken for a function to
+    reach a value of :math:`1/e`.
+
+    This function finds the time steps between which this value falls. The error
+    of the function is the distance between timesteps at that point.
+
+    Args:
+        time (class:`np.ndarray`): The timesteps corresponding to each msd value.
+        value (class:`np.ndarray`): Values of the relaxation paramter
+
+    Returns:
+        diffusion_constant (float): The relaxation time for the given quantity.
+        error (float): The error in the fit of the relaxation
+
+    """
+    index = np.argmax(value < 1/np.exp(1))
+    return time[index], time[index]-time[index-1]
+
+
+def exponential_relaxation(time: np.ndarray,
+                           value: np.ndarray,
+                           sigma: np.ndarray=None,
+                           value_width: float=0.3) -> Tuple[float, float]:
+    """Fit a region of the exponential relaxation with an exponential.
+
+    This fits an exponential to the small region around the value 1/e.
+
+    Returns:
+        relaxation_time (float): The relaxation time for the given quantity
+        error_min (float): The minmum error value
+        error_max (float): The maximum error value
+
+    """
+    exp_value = 1/np.exp(1)
+    fit_region = np.logical_and((exp_value - value_width/2) < value,
+                                (exp_value + value_width/2) > value)
+    logger.debug('Num elements: %d', np.sum(fit_region))
+    zero_est = time[np.argmin(np.abs(value - exp_value))]
+    if sigma is not None:
+        sigma = sigma[fit_region]
+    popt, pcov = curve_fit(
+        _exponential_decay,
+        time[fit_region],
+        value[fit_region],
+        p0=[1., 1/zero_est],
+        sigma=sigma,
+    )
+    perr = 2*np.sqrt(np.diag(pcov))
+    logger.debug('Fit Parameters: %s', popt)
+
+    def find_root(a, b):
+        return newton(
+            _exponential_decay,
+            args=(a, b, -exp_value),
+            x0=zero_est,
+            fprime=_ddx_exponential_decay,
+        )
+
+    val_mean = find_root(*popt)
+    val_min = find_root(*(popt-perr))
+    val_max = find_root(*(popt+perr))
+    return val_mean, val_mean - val_min, val_max - val_min
