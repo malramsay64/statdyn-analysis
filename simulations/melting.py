496e5ea92b9c33a195e85fd8cbc431c13e22ea2d
Malcolm Ramsay <malramsay64@gmail.com>
2017-10-17 14:06:34 +1100

    Use itertools to create array jobs
    To make it a lot cleaner when submitting jobs I have modified the
    submission file to use the pbs job arrays. This is much easier in python
    as I can use the `itertools.product` function to easily create all the
    possible permutations of the values I specify.

--- simulations/melting.py
+++ simulations/melting.py
@@ -8,6 +8,7 @@
 
 """Set running a series of simulations."""
 
+import itertools
 import os
 import subprocess
 import sys
@@ -16,29 +17,40 @@ from pathlib import Path
 import numpy as np
 
 pbs_file = """#!/usr/bin/env python
-#PBS -N Trimer-P{P:.2f}-T{T:.2f}-I{I:.2f}
+#PBS -N Trimer
 #PBS -m abe
 #PBS -M malramsay64+quartz@gmail
 #PBS -j oe
-#PBS -o {outdir}/pbs-P{P:.2f}-T{T:.2f}-I{I:.2f}.log
+#PBS -o {outdir}/pbs.log
 #PBS -l select=1:ncpus={ncpus}
 #PBS -l walltime=500:00:00
 #PBS -l cput=3600:00:00
 #PBS -V
+{array_flag}
 
 import subprocess
 import os
 from pathlib import Path
 
-temperature = {T}
-pressure = {P}
-moment_inertia = {I}
+def create_fname(mol, temp, pressure, mom_I, crys):
+    return mol + '-P' + pressure + '-T' + temp + '-I' + mom_I + '-' + crys + '.gsd'
+
+all_values = {values}
+
+job_index = int(os.environ.get('PBS_ARRAY_INDEX', 0))
+
+temperature, pressure, moment_inertia, crystal = all_values[job_index]
+
+temperature = '{{:.2f}}'.format(temperature)
+pressure = '{{:.2f}}'.format(pressure)
+moment_inertia = '{{:.2f}}'.format(moment_inertia)
+
 ncpus = {ncpus}
 
 common_opts = [
-    '--pressure', str(pressure),
-    '--temperature', str(temperature),
-    '--moment-inertia-scale', str(moment_inertia),
+    '--pressure', pressure,
+    '--temperature', temperature,
+    '--moment-inertia-scale', moment_inertia,
     '--output', '{outdir}',
 ]
 
@@ -49,23 +61,24 @@ if ncpus > 1:
         '--np', str(ncpus),
     ] + run_comand
 
-create_out = '{outdir}/Trimer-P{P:.2f}-T0.2-I{I:.2f}-p2.gsd'
+create_temp = '0.2'
+create_out = '{outdir}' + create_fname('Trimer', create_temp, pressure, moment_inertia, crystal)
 
 create_opts = [
-    '--space-group', 'p2',
+    '--space-group', crystal,
     '--steps', '1000',
-    '--temperature', '0.2',
+    '--temperature', create_temp,
     create_out,
 ]
 
 subprocess.call(run_comand + ['create'] + common_opts + create_opts)
 
-equil_out = '{outdir}/Trimer-P{P:.2f}-T{T:.2f}-I{I:.2f}-p2.gsd'
+equil_out = '{outdir}' + create_fname('Trimer', temperature, pressure, moment_inertia, crystal)
 
 equil_opts = [
     '--equil-type', 'crystal',
-    '--init-temp', '0.2',
-    '--steps', '100000',
+    '--init-temp', create_temp,
+    '--steps', '1000',
     create_out,
     equil_out,
 ]
@@ -83,31 +96,39 @@ subprocess.call(run_comand + ['prod'] + common_opts + prod_opts)
 
 
 temperatures = np.arange(0.2, 0.4, 0.2)
-pressures = np.arange(1.5, 3.0, 1.5)
+pressures = np.arange(1.5, 4.5, 1.5)
 mom_inertia = np.power(10., np.arange(0, 1))
+crystals = ['p2']
 
-outdir = Path.home() / 'tmp1m/2017-10-17-testing'
+outdir = Path.home() / 'tmp1m/2017-10-17-test2'
 
 if __name__ == "__main__":
     # ensure outdir exists
     outdir.mkdir(exist_ok=True)
 
+    all_values = list(itertools.product(temperatures, pressures, mom_inertia, crystals))
 
+    def get_array_flag(num_values: int) -> str:
+        if num_values == 1:
+            return ''
+        else:
+            return f'#PBS -J 0-{num_values-1}'
 
-    for T in temperatures:
-        for P in pressures:
-            for I in mom_inertia:
-                cat_file = subprocess.Popen(
-                    ['echo', pbs_file.format(T=T, P=P, I=I, outdir=outdir, ncpus=8, version='dev', home=Path.home())],
-                    stdout=subprocess.PIPE)
+    cat_file = subprocess.Popen(['echo', pbs_file.format(values=all_values,
+                                 array_flag=get_array_flag(len(all_values)),
+                                 outdir=outdir,
+                                 ncpus=8)], stdout=subprocess.PIPE)
 
-                subprocess.Popen(['qsub'],
-                                 stdin=cat_file.stdout,
-                                 stdout=sys.stdout,
-                                 stderr=sys.stderr,
-                                 env=os.environ,
-                                 )
+    subprocess.Popen(['qsub'],
+                     stdin=cat_file.stdout,
+                     stdout=sys.stdout,
+                     stderr=sys.stderr,
+                     env=os.environ,
+                     )
 
-                with open('testfile.py', 'w') as tf:
-                    tf.write(pbs_file.format(T=T, P=P, I=I, outdir=outdir, ncpus=8, version='dev', home=Path.home()))
-                cat_file.stdout.close()
+    with open('testfile.py', 'w') as tf:
+        tf.write(pbs_file.format(values=all_values,
+                                 array_flag=get_array_flag(len(all_values)),
+                                 outdir=outdir,
+                                 ncpus=8))
+    cat_file.stdout.close()
