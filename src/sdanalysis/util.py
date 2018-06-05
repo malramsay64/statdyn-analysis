ee67c989629d5c499263875d1ff5534b17284f05
Malcolm Ramsay <malramsay64@gmail.com>
2018-06-05 12:49:08 +1000

    ENH Improved error handling parsing filename
    The function retrieving simulation data from filenames is rather
    brittle, failing on cases which don't exactly match the desired input.
    This commit modifies the function to be somewhat more resilient,
    although there is still work to be done in this regards.

--- src/sdanalysis/util.py
+++ src/sdanalysis/util.py
@@ -22,13 +22,24 @@ class variables(NamedTuple):
 def get_filename_vars(fname: Path):
     fname = Path(fname)
     flist = fname.stem.split("-")
-    if len(flist) < 3:
+    if len(flist) < 4:
         return variables(None, None, None)
 
-    pressure = flist[2][1:]
-    temp = flist[3][1:]
-    try:
-        crys = flist[4]
-    except IndexError:
+    pressure_str = flist[2]
+    if pressure_str[0] == "P":
+        pressure: Optional[str] = pressure_str[1:]
+    else:
+        pressure = None
+
+    temperature_str = flist[3]
+    if temperature_str[0] == "T":
+        temp: Optional[str] = temperature_str[1:]
+    else:
+        temp = None
+
+    if len(flist) >= 5:
+        crys: Optional[str] = flist[4]
+    else:
         crys = None
+
     return variables(temp, pressure, crys)
