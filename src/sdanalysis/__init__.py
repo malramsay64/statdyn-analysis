ba9a73aca2282f9beeff5d8c22b8a3d91400172f
Malcolm Ramsay <malramsay64@gmail.com>
2018-07-27 09:06:25 +1000

    ENH Simplify common imports from modules
    This groups the common imports into the __init__.py files allowing for
    a simpler use case for interactive coding. With all the key components
    in the init files, the imports can be grouped together.
    
    This commit fixes #16

--- src/sdanalysis/__init__.py
+++ src/sdanalysis/__init__.py
@@ -1,1 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
 
+from .figures.configuration import plot_frame
+from .frame import Frame, HoomdFrame, LammpsFrame
+from .molecules import Dimer, Disc, Molecule, Trimer
+from .params import SimulationParams
+from .version import __version__
---
+++ src/sdanalysis/figures/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+from .configuration import plot_frame
