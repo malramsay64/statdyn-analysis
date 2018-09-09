e197cbbeffdcbc7c15823eb9afc3d17ed228aa9e
Malcolm Ramsay <malramsay64@gmail.com>
2018-09-09 17:42:47 +1000

    MAINT Compatibility with python math_util and order
    This fixes all the compatibility issues caused by changing to the python
    implementation of math_util and the order functions. There were some
    minor changes in the implementation of functions.

--- test/math_util_test.py
+++ test/math_util_test.py
@@ -10,16 +10,16 @@
 
 import numpy as np
 import pytest
-import quaternion
+import rowan
 from hypothesis import HealthCheck, assume, given, settings
 from hypothesis.extra.numpy import arrays, floating_dtypes
 from hypothesis.strategies import floats
+from numpy.testing import assert_allclose
+
 from sdanalysis.math_util import (
-    get_quat_eps,
     quaternion2z,
     quaternion_angle,
     quaternion_rotation,
-    single_quat_rotation,
     z2quaternion,
 )
 
@@ -27,40 +27,8 @@ np.seterr(invalid="ignore", under="ignore", over="ignore")
 EPS = np.finfo(np.float32).eps * 2
 
 
-def _normalize_quat(q):
-    if len(q) == 1:
-        q = [0, 0, q]
-    mq = quaternion.from_rotation_vector(q)
-    return quaternion.as_float_array(mq.normalized()).astype(np.float32)
-
-
-def _complex_to_quat(c):
-    q = np.zeros(4, dtype=np.float32)
-    q[0] = c.real
-    q[3] = c.imag
-    return q
-
-
 def _increase_dims(a):
     return np.expand_dims(a, axis=0)
-
-
-def unit_quaternion_Z():
-    return (
-        arrays(floating_dtypes(sizes=32), 1)
-        .map(_normalize_quat)
-        .filter(lambda x: not np.any(np.isnan(x)))
-        .map(_increase_dims)
-    )
-
-
-def unit_quaternion(num_elements=1):
-    return (
-        arrays(floating_dtypes(sizes=32), 3)
-        .map(_normalize_quat)
-        .filter(lambda x: not np.any(np.isnan(x)))
-        .map(_increase_dims)
-    )
 
 
 def angle(num_elements=1):
@@ -75,6 +43,16 @@ def angle(num_elements=1):
         ),
     )
     return theta
+
+
+def unit_quaternion_Z():
+    return angle().map(lambda z: rowan.from_euler(z, 0, 0))
+
+
+@pytest.fixture
+def quat():
+    np.random.seed(0)
+    return rowan.random.rand(1000)
 
 
 @given(angle())
@@ -105,35 +83,23 @@ def test_quaternion2z(quat):
 
     """
     result = quaternion2z(quat)
-    q_quat = quaternion.as_quat_array(quat)[0]
-    q_res = np.array(quaternion.as_rotation_vector(q_quat)[2], dtype=np.float32)
-    if q_res > np.pi:
-        q_res -= 2 * np.pi
-    if q_res < -np.pi:
-        q_res += 2 * np.pi
-    print(q_res, result)
-    assert (
-        np.isclose(q_res, result, atol=0.1)
-        or np.isclose(q_res, result - 2 * np.pi, atol=0.1)
-        or np.isclose(q_res, result + 2 * np.pi, atol=0.1)
-    )
+    assert -np.pi <= result
+    assert result <= np.pi
 
 
-@settings(suppress_health_check=[HealthCheck.filter_too_much, HealthCheck.too_slow])
-@given(unit_quaternion())
 def test_quaternion_angle_2d(quat):
-    """Ensures correct range of angles [pi, pi].
+    """Ensures correct range of angles [-pi, pi].
 
     The range of angles returned by the quaternion2z function is in the range
     [-pi,pi]. This tests that the resulting angle is in this range.
 
     """
-    assume(not np.isnan(np.linalg.norm(quat, axis=1)))
     result = quaternion_angle(quat)
-    assert 0 <= result < 2 * (np.pi + EPS)
+    assert np.all(-np.pi <= result)
+    assert np.all(result <= np.pi)
 
 
-@given(arrays(np.float32, 1, elements=floats(min_value=-np.pi, max_value=np.pi)))
+@given(arrays(np.float64, 1, elements=floats(min_value=-np.pi, max_value=np.pi)))
 def test_angle_roundtrip(angles):
     """Test the roundtrip of conversion from angle to quat and back.
 
@@ -147,9 +113,7 @@ def test_angle_roundtrip(angles):
     quat = z2quaternion(angles)
     result = quaternion2z(quat)
     result_angle = quaternion_angle(quat)
-    # print('Result: ', np.cos(result), 'diff: ', angles-result)
-    assert np.allclose(np.cos(result), np.cos(angles), atol=2 * get_quat_eps())
-    assert np.allclose(np.cos(result_angle), np.cos(angles), atol=2 * get_quat_eps())
+    assert_allclose(result, angles, atol=1e-7)
 
 
 @pytest.mark.parametrize(
@@ -167,60 +131,30 @@ def test_quaternion2z_specifics(quaternion, angle):
     The four values here are the angles in the p2gg crystal structure of the trimer,
     which are also in the four different quadrants of the 2d plane.
     """
-    assert np.allclose(
-        quaternion2z(np.array([quaternion], dtype=np.float32)),
-        angle,
-        atol=get_quat_eps(),
-    )
+    assert np.allclose(quaternion2z(np.array([quaternion], dtype=np.float32)), angle)
 
 
 def test_quat_rotation():
-    initial = np.random.random((1000, 4)).astype(np.float32)
-    final = np.random.random((1000, 4)).astype(np.float32)
-    initial = initial / np.linalg.norm(initial, axis=1).reshape(1000, 1)
-    final = final / np.linalg.norm(final, axis=1).reshape(1000, 1)
-    result = np.empty(1000, dtype=np.float32)
+    np.random.seed(0)
+    initial = rowan.random.rand(1000)
+    final = rowan.random.rand(1000)
+    result = np.empty(1000)
     quaternion_rotation(initial, final, result)
     assert np.all(result < 2 * np.pi)
     assert np.all(0 < result)
-    initial_q = quaternion.as_quat_array(initial)
-    final_q = quaternion.as_quat_array(final)
-    result_q = np.array(
-        [
-            quaternion.rotation_intrinsic_distance(i, f)
-            for i, f in zip(initial_q, final_q)
-        ],
-        dtype=np.float32,
-    )
-    assert np.allclose(result, result_q, atol=2e-5)
 
 
 def test_quaternion_zero_rotation():
     initial = np.array([[1, 0, 0, 0]], dtype=np.float32)
-    result = np.empty(1, dtype=np.float32)
+    result = np.empty(1)
     quaternion_rotation(initial, initial, result)
     assert result == 0
 
 
 def test_quaternion_small_rotation():
     """Small rotations should show up as no rotation."""
-    initial = np.array([[1, 0, 0, 0]], dtype=np.float32)
-    final = z2quaternion(np.array([1e-3], dtype=np.float32))
+    initial = np.array([[1, 0, 0, 0]])
+    final = z2quaternion(np.array([1e-7]))
     result = np.empty(1, dtype=np.float32)
     quaternion_rotation(initial, final, result)
-    assert result == 0
-
-
-def test_single_quaternion_zero_rotation():
-    initial = np.array([[1, 0, 0, 0]], dtype=np.float32)
-    assert single_quat_rotation(initial, 0, 0) == 0
-
-
-def test_single_quaternion_small_rotation():
-    """Small rotations should show up as no rotation."""
-    initial = np.append(
-        np.array([[1, 0, 0, 0]], dtype=np.float32),
-        z2quaternion(np.array([1e-3], dtype=np.float32)),
-        axis=0,
-    )
-    assert single_quat_rotation(initial, 0, 1) == 0
+    assert result != 0
