BUG Handle NaN values by filtering not averaging
I made the assessment that for the Molecular Relaxations, an initial
frame which has any NaN values for the relaxation time is invalid, since
it has not had time to properly equilibrate. In handling these values I
was taking the mean over the configuration, which when there were any
NaN values, the resulting mean would be NaN. However, also calculating
the harmonic mean caused issues, since I needed the values for all the
molecules and not just for each frame.

To fix the issue I am now filtering for the NaN values before
calculating both the mean and the harmonic mean, ensuring both these
quantities have valid values.

src/sdanalysis/relaxation.py
@@ -184,7 +184,7 @@ def exponential_relaxation(
     try:
         p0 = (1., 1 / zero_est)
     except (ZeroDivisionError, FloatingPointError) as err:
-        logger.exception("%s", err)
+        logger.warning("Handled exception in estimating zero\n%s", err)
         p0 = (1., 0.)
 
     if sigma is not None:
@@ -194,8 +194,8 @@ def exponential_relaxation(
         popt, pcov = curve_fit(
             _exponential_decay, time[fit_region], value[fit_region], p0=p0, sigma=sigma
         )
-    except RuntimeError as err:
-        logger.exception("%s", err)
+    except (RuntimeError, FloatingPointError) as err:
+        logger.warning("Exception in fitting curve, returning Nan\n%s", err)
         return Result(np.nan, np.nan)
 
     try:
@@ -222,13 +222,13 @@ def exponential_relaxation(
             val_min: float = find_root(*(popt - perr))
             val_max: float = find_root(*(popt + perr))
         except FloatingPointError as err:
-            logger.exception("%s", err)
+            logger.warning("Handled Exception in calculating Error bars\n%s", err)
             val_min = 0
             val_max = val_mean - val_min
         return Result(val_mean, val_max - val_min)
 
     except RuntimeError as err:
-        logger.exception("%s", err)
+        logger.warning("Failed to converge on value, returning NaN")
         return Result(np.nan, np.nan)
 
 
@@ -386,17 +386,28 @@ def compute_relaxations(infile) -> None:
             translate_relaxation(quantity) for quantity in relaxations.columns
         ]
 
-    df_mol = pandas.read_hdf(infile, "molecular_relaxations")
-    df_mol.replace(2 ** 32 - 1, np.nan, inplace=True)
-    df_mol.index.names = ["init_frame", "molecule"]
-    df_mol = df_mol.groupby(["init_frame", "temperature", "pressure"]).agg(np.mean)
-    # Initial frames with any nan value are excluded from analysis. It is assumed they
-    # didn't run for a long enough time.
-    df_mol = df_mol.dropna()
-    df_mol = df_mol.groupby(["temperature", "pressure"]).agg(["mean", hmean])
-    df_mol.columns = ["_".join(f) for f in df_mol.columns.tolist()]
+    df_mol = compute_molecular_relaxations(
+        pandas.read_hdf(infile, "molecular_relaxations")
+    )
 
     df_all = df_mol.join(relaxations, on=["temperature", "pressure"]).reset_index()
     assert "temperature" in df_all.columns
     assert "pressure" in df_all.columns
     df_all.to_hdf(infile, "relaxations")
+
+
+def compute_molecular_relaxations(df: pandas.DataFrame) -> pandas.DataFrame:
+    assert "temperature" in df.columns
+    assert "pressure" in df.columns
+    assert len(df.index.names) == 2
+
+    df.replace(2 ** 32 - 1, np.nan, inplace=True)
+    df.index.names = ["init_frame", "molecule"]
+    # Initial frames with any NaN value are excluded from analysis. It is assumed they
+    # didn't run for a long enough time.
+    df = df.groupby(["init_frame", "temperature", "pressure"]).filter(
+        lambda x: x.isna().sum().sum() == 0
+    )
+    df = df.groupby(["temperature", "pressure"]).agg(["mean", hmean])
+    df.columns = ["_".join(f) for f in df.columns.tolist()]
+    return df

test/relaxation_test.py
@@ -12,7 +12,7 @@
 import numpy as np
 import pandas
 import pytest
-from hypothesis import given, settings
+from hypothesis import example, given
 from hypothesis.extra.numpy import arrays
 
 from sdanalysis import relaxation
@@ -133,6 +133,7 @@ def test_compute_relaxations_values(dynamics_file):
 
 
 @given(values=arrays(dtype=np.float32, shape=1000))
+@example(values=np.ones(1000))
 @pytest.mark.parametrize("relax_type", relaxation_types)
 def test_compute_relaxations_random(values, relax_type):
     timesteps = np.arange(values.shape[0], dtype=int)
@@ -158,3 +159,29 @@ def test_series_relaxations_index(indexes):
     df = pandas.DataFrame(data).groupby(indexes).mean()
     s = df.test
     relaxation.series_relaxation_value(s)
+
+
+@pytest.fixture
+def relax_df():
+    # Use one frame to detect when there is incorrect handling of the harmonic mean
+    frames = 1
+    mols = 100
+    np.random.seed(0)
+    df = pandas.DataFrame(
+        {
+            "init_frame": np.repeat(np.arange(frames), mols),
+            "molecule": np.tile(np.arange(mols), frames),
+            "temperature": 0.1,
+            "pressure": 0.1,
+            "tau_DL04": np.random.random(frames * mols),
+            "tau_D04": np.random.random(frames * mols),
+        }
+    )
+    return df.groupby(["init_frame", "molecule"]).mean()
+
+
+def test_relaxation_hmean(relax_df):
+    df = relaxation.compute_molecular_relaxations(relax_df)
+    columns = ["tau_DL04", "tau_D04"]
+    for col in columns:
+        assert np.all(df[col + "_mean"].values != df[col + "_hmean"].values)