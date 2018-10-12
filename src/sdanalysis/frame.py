BUG Return a quaternion with 0 rotation from LammpsFrame
The LammpsFrame class was returning all zeros for the orientation which
is an invalid value for a quaternion. This uses the 'zero' quaternion
[1, 0, 0, 0] for the orientation of all molecules instead.

src/sdanalysis/frame.py
@@ -80,7 +80,9 @@ class LammpsFrame(Frame):
 
     @property
     def orientation(self) -> np.ndarray:
-        return np.zeros((len(self), 4), dtype=np.float32)
+        orient = np.zeros((len(self), 4), dtype=np.float32)
+        orient[:, 0] = 1
+        return orient
 
     @property
     def timestep(self) -> int: