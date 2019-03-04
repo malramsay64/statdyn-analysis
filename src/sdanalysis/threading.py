78f8fcb97cd06481bc886895c7942b4f936a4741
Malcolm Ramsay <malramsay64@gmail.com>
2019-03-04 13:48:47 +1100

    feat: Specify number of threads for parallel processing function
    The function for processing the simulations on multiple cores now takes
    an argument for the number of cpus to use.

--- src/sdanalysis/threading.py
+++ src/sdanalysis/threading.py
@@ -15,18 +15,21 @@ the rest of the implementation.
 from copy import deepcopy
 from multiprocessing import Manager, Pool, Queue, cpu_count
 from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Tuple, Optional
 
 import pandas
 
 from .params import SimulationParams
 from .read import process_file
+import logging
 
+logger = logging.getLogger(__name__)
 
 def file_writer(queue: Queue, outfile: Path):
     with pandas.HDFStore(outfile, "w") as dst:
         while True:
             group, dataset = queue.get()
+            logger.debug("Writing dataset to %s", dataset)
             if dataset is None:
                 break
             dst.append(group, dataset)
@@ -42,13 +45,20 @@ def parallel_process_files(
     input_files: Tuple[str],
     sim_params: SimulationParams,
     relaxations: List[Dict[str, Any]] = None,
+    num_cpus: Optional[int] = None,
 ) -> None:
     # The manager queue needs to be used
     manager = Manager()
     queue = manager.Queue()
 
     # Number of cpus + an additional for the writer file
-    with Pool(cpu_count() + 1) as pool:
+    if num_cpus is None:
+        num_cpus = cpu_count() + 1
+    # When there are fewer than 2 threads the processing never starts
+    if num_cpus < 2:
+        num_cpus = 2
+
+    with Pool(num_cpus) as pool:
 
         # Put file writing process to work first
         writer = pool.apply_async(file_writer, (queue, sim_params.outfile))
