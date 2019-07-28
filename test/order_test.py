480db65008c13d9e3a4df78c884a870e613f6371
Malcolm Ramsay <malramsay64@gmail.com>
2019-07-29 08:55:55 +1000

    test: Use read module to read files for order test
    This uses the internal read module to read the files which allows me to
    use the Frame properties in the order testing, significantly simplifying
    the code.

--- test/order_test.py
+++ test/order_test.py
@@ -11,11 +11,10 @@
 
 """Ensure correctness of the order parameters."""
 
-import gsd.hoomd
 import numpy as np
 import pytest
 
-from sdanalysis import order
+from sdanalysis import order, read
 
 INFILES = [
     "test/data/dump-Trimer-13.50-0.40-p2.gsd",
@@ -26,16 +25,15 @@ INFILES = [
 
 @pytest.fixture(scope="module", params=INFILES)
 def frame(request):
-    with gsd.hoomd.open(request.param, "rb") as f:
-        yield f[0]
+    return next(read.open_trajectory(request.param))
 
 
 def test_compute_neighbours(frame):
     max_radius = 10
     max_neighbours = 6
-    num_mols = frame.particles.N
+    num_mols = len(frame)
     neighs = order.compute_neighbours(
-        frame.configuration.box, frame.particles.position, max_radius, max_neighbours
+        frame.box, frame.position, max_radius, max_neighbours
     )
     assert neighs.shape == (num_mols, max_neighbours)
     assert np.all(neighs < num_mols)
@@ -43,40 +41,41 @@ def test_compute_neighbours(frame):
 
 def test_num_neighbours(frame):
     max_radius = 3.5
-    neighs = order.num_neighbours(
-        frame.configuration.box, frame.particles.position, max_radius
-    )
+    neighs = order.num_neighbours(frame.box, frame.position, max_radius)
     assert np.all(neighs == 6)
 
 
 def test_voronoi_neighbours(frame):
-    neighs = order.compute_voronoi_neighs(
-        frame.configuration.box, frame.particles.position
-    )
+    neighs = order.compute_voronoi_neighs(frame.box, frame.position)
     assert np.all(neighs == 6)
+
+
+def test_create_neigh_ordering(frame):
+    order_func = order.create_neigh_ordering(6)
+    assert np.all(order_func(frame))
 
 
 def test_orientational_order(frame):
     max_radius = 3.5
     orient_order = order.orientational_order(
-        frame.configuration.box,
-        frame.particles.position,
-        frame.particles.orientation,
-        max_radius,
+        frame.box, frame.position, frame.orientation, max_radius
     )
     assert np.all(orient_order > 0.60)
 
 
+def test_create_orient_ordering(frame):
+    order_func = order.create_orient_ordering(0.60)
+    assert np.all(order_func(frame))
+
+
 def test_relative_distance(frame):
-    print(frame.configuration.box)
-    distances = order.relative_distances(
-        frame.configuration.box, frame.particles.position
-    )
+    print(frame.box)
+    distances = order.relative_distances(frame.box, frame.position)
     assert np.all(np.isfinite(distances))
 
 
 def test_relative_orientations(frame):
     orientations = order.relative_orientations(
-        frame.configuration.box, frame.particles.position, frame.particles.orientation
+        frame.box, frame.position, frame.orientation
     )
     assert np.all(np.isfinite(orientations))
