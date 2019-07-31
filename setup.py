2d02176a6e31da0c68acd8715d94aa7882c813c2
Malcolm Ramsay <malramsay64@gmail.com>
2019-07-31 14:41:15 +1000

    chore(deps): Define numpy 1.14 as oldest version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def read(path, encoding="utf-8"):
 
 
 install_requires = [
-    "numpy>=1.15,<1.18",
+    "numpy>=1.14,<1.18",
     "scipy~=1.0",
     "scikit-learn>=0.20.0,<1.0",
     "pandas>=0.23,<0.26",
