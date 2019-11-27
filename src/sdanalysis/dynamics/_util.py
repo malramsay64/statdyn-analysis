fix: Calculation of structural relaxation
This fixes the calculation of the structural relaxation

Fixes #155

src/sdanalysis/dynamics/_util.py
@@ -42,11 +42,16 @@ def create_wave_vector(wave_number: float, angular_resolution: int):
 def molecule2particles(
     position: np.ndarray, orientation: np.ndarray, mol_vector: np.ndarray
 ) -> np.ndarray:
+    if isinstance(orientation, tuple):
+        raise ValueError("Expecting numpy array found tuple", orientation)
     if np.allclose(orientation, 0.0):
         orientation[:, 0] = 1.0
-    return np.concatenate(
+    rotated = np.concatenate(
         [rotate_vectors(orientation, pos) for pos in mol_vector.astype(np.float32)]
-    ) + np.repeat(position, mol_vector.shape[0], axis=0)
+    )
+    translated = np.repeat(position, mol_vector.shape[0], axis=0)
+
+    return rotated + translated
 
 
 def _static_structure_factor(

src/sdanalysis/dynamics/dynamics.py
@@ -298,21 +298,15 @@ class Dynamics:
             raise ValueError(
                 "The wave number is required for the structural relaxation."
             )
-        return structural_relax(
-            np.linalg.norm(
-                molecule2particles(
-                    self.delta_translation,
-                    rowan.from_euler(
-                        self.delta_rotation[:, 0],
-                        self.delta_rotation[:, 1],
-                        self.delta_rotation[:, 2],
-                    ),
-                    self.mol_vector,
-                ),
-                axis=1,
-            ),
-            self.distance,
+        quat_rot = rowan.from_euler(
+            self.delta_rotation[:, 0],
+            self.delta_rotation[:, 1],
+            self.delta_rotation[:, 2],
         )
+        atomic_motion = molecule2particles(
+            self.delta_translation, quat_rot, self.mol_vector
+        )
+        return structural_relax(np.linalg.norm(atomic_motion, axis=1), self.distance)
 
     def compute_all(
         self,
@@ -357,7 +351,7 @@ class Dynamics:
         # The structural relaxation requires the distance value to be set
         if self.distance is not None:
             dynamic_quantities["com_struct"] = structural_relax(
-                self.delta_translation, dist=self.distance
+                self.compute_displacement(), dist=self.distance
             )
 
         dynamic_quantities["mean_rotation"] = self.compute_mean_rotation()
@@ -366,7 +360,7 @@ class Dynamics:
         dynamic_quantities["alpha_rot"] = self.compute_alpha_rot()
         dynamic_quantities["gamma"] = self.compute_gamma()
         dynamic_quantities["overlap"] = mobile_overlap(
-            self.delta_translation, self.delta_rotation
+            self.compute_displacement(), self.compute_rotation()
         )
 
         # The structural relaxation of all atoms is the most complex.

test/dynamics_test.py
@@ -19,8 +19,7 @@ from freud.box import Box
 from hypothesis import assume, example, given
 from hypothesis.extra.numpy import arrays
 from hypothesis.strategies import floats
-
-from sdanalysis import HoomdFrame, dynamics, read
+from sdanalysis import HoomdFrame, Trimer, dynamics, read
 
 MAX_BOX = 20.0
 EPS = 4 * np.sqrt(np.finfo(np.float32).eps)
@@ -304,3 +303,30 @@ def test_large_rotation():
             rowan.from_euler(np.ones(10) * np.pi / 4 * i, np.zeros(10), np.zeros(10)),
         )
     assert np.allclose(np.linalg.norm(dyn.delta_rotation, axis=1), np.pi / 4 * 9)
+
+
+@given(
+    arrays(np.float64, (10, 2), floats(-4, 4)),
+    arrays(np.float64, (10, 3), floats(-np.pi / 3, np.pi / 3)),
+)
+def test_struct(translation, rotation):
+    translation = np.append(translation, np.zeros((10, 1)), axis=1)
+    init_pos = np.zeros((10, 3))
+    init_orient = rowan.from_euler(np.zeros(10), np.zeros(10), np.zeros(10))
+    dyn = dynamics.Dynamics(
+        0, np.ones(3) * 10, init_pos, init_orient, wave_number=2.9, molecule=Trimer()
+    )
+
+    orientation = rowan.from_euler(rotation[:, 0], rotation[:, 1], rotation[:, 2])
+    dyn.add(translation, orientation)
+
+    motion = np.linalg.norm(
+        dynamics._util.molecule2particles(translation, orientation, Trimer().positions),
+        axis=1,
+    )
+    print(dyn.delta_translation)
+    assert np.allclose(dyn.delta_rotation, rotation)
+    assert np.allclose(dyn.delta_translation[:, :2], translation[:, :2], atol=1e-7)
+    assert np.allclose(
+        dyn.compute_struct_relax(), (motion < dyn.distance).mean(), atol=2e-7
+    )