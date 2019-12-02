fix: Calculate the sum of the rotation axes not the mean
I was calculating the mean of the rotation axes rather than the mean,
which will have resulted in the rotations being 1/3 of what they should
have been for the squared displacement.

src/sdanalysis/dynamics/dynamics.py
@@ -218,7 +218,7 @@ class Dynamics:
 
     def compute_rotation2(self) -> np.ndarray:
         """Compute the rotation from the initial frame."""
-        return np.square(self.delta_rotation).mean(axis=1)
+        return np.square(self.delta_rotation).sum(axis=1)
 
     def compute_mean_rotation(self) -> float:
         """Compute the rotation from the initial frame."""