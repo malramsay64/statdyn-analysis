250012d22f68cc36d9942ba28d37b5621edcc4f3
Malcolm Ramsay <malramsay64@gmail.com>
2020-01-15 15:29:50 +1100

    chore(deps): Update range for hypothesis
    This updates the range of allowed versions of hypothesis to support
    version 5.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 test_requires = [
     "pytest>=5.0.1,<5.4.0",
     "pylint>=2.4.3,<2.5",
-    "hypothesis>=4.43.1,<5.0",
+    "hypothesis>=4.43.1,<6.0",
     "coverage==5.0.1",
     "isort==4.3.21",
     "black==19.10b0",
